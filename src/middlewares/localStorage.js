4429f86ae868ddf6e17faf7a75281ac0fc11a587
Antoine Brisseau <antoinebrisseau@gmail.com>
2017-04-12 02:39:57 +0200

    Add message history

--- src/middlewares/localStorage.js
+++ src/middlewares/localStorage.js
@@ -9,6 +9,9 @@ export function localStorageMiddleware() {
   return () => next => action => {
       if (!action.fromLocalStorage) {
         localStorage.setItem(storageKey, JSON.stringify(action))
+        let messages = JSON.parse(localStorage.getItem('messages'))
+        messages.push(action)
+        localStorage.setItem('messages', JSON.stringify(messages))
       }
       next(action)
   }
@@ -16,6 +19,9 @@ export function localStorageMiddleware() {
 
 export function createStorageListener(store) {
   return event => {
+    if (event.key !== storageKey) {
+      return
+    }
     const receivedAction = JSON.parse(event.newValue)
     if (receivedAction.tabId !== tabId) {
       receivedAction.fromLocalStorage = true
