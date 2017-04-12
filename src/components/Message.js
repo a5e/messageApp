1afc84fc37a351b5a0ce22039ea56e7aa2058538
Antoine Brisseau <antoinebrisseau@gmail.com>
2017-04-12 02:44:07 +0200

    Fix propTypes warning

--- src/components/Message.js
+++ src/components/Message.js
@@ -1,4 +1,5 @@
-import React, { PropTypes } from 'react'
+import React from 'react'
+import PropTypes from 'prop-types'
 
 const Message = (message) => {
   let isMe = message.tabId === sessionStorage.getItem('tab-id') ? ' (moi)' : '';
